4aedcb1166b5331ca29ad89415bce44ca8326413
Matt Day <mattsday@google.com>
2020-03-13 11:46:48 +0000

    Tidied up a little

--- src/main/kotlin/uk/co/mattsday/demos/hellocloudrun/HelloCloudRunApplication.kt
+++ src/main/kotlin/uk/co/mattsday/demos/hellocloudrun/HelloCloudRunApplication.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PathVariable
 
 @SpringBootApplication
 class HelloCloudRunApplication
@@ -12,10 +13,17 @@ fun main(args: Array<String>) {
 	runApplication<HelloCloudRunApplication>(*args)
 }
 
+// Keeping it all in one file
 @RestController
-class demo {
-	@GetMapping("hello")
+class Demo {
+	// Listen on /hello and /
+	@GetMapping(value = ["hello", "/"])
 	fun hello() : List<String> {
 		return listOf("hello", "cloud", "run")
+	}
+
+	@GetMapping("hello/{name}")
+	fun helloYou(@PathVariable("name") name :String) : List<String> {
+		return listOf("hello", name)
 	}
 }
